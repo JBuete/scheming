c57aec7bd64057d4981dab18f0dbe8b09efab7f7
Jacob Buete <jacob.buete@anu.edu.au>
2020-05-29 15:47:57 +1000

    [feat] Implement Colourblind object
    - The Colourblind object has been created and can convert
      any colour into the percieved colour for 8 different types
      of colour blindness.

--- colours.py
+++ colours.py
@@ -171,6 +171,162 @@ _xyz_srgb_matrix = numpy.array([[3.2404542, -1.5371385, -0.4985314],
                                 [-0.9692660,  1.8760108,  0.0415560],
                                 [0.0556434, -0.2040259,  1.0572252]])
 
+_blindness_type = {"protan": {"x": 0.7465,
+                              "y": 0.2535,
+                              "m": 1.273463,
+                              "yi": -0.073894},
+                   "deutan": {"x": 1.4000,
+                              "y": -0.400,
+                              "m": 0.968437,
+                              "yi": 0.003331},
+                   "tritan": {"x": 0.1748,
+                              "y": 0.0000,
+                              "m": 0.062921,
+                              "yi": 0.292119}}
+
+
+class Colourblind():
+    """A colourblind object."""
+
+    def __init__(self, rgb, illuminant="D65", linear=True):
+        # first thing is to convert the RGB
+        if linear:
+            self.linear_rgb = numpy.array(rgb)
+            self.rgb = self._linear_to_RGB()
+        else:
+            self.rgb = numpy.array(rgb)
+            self.linear_rgb = self._RGB_to_linear()
+
+        # now that we've done that we need to convert the linear RGB to XYZ
+        self.xyz = self._rgb_to_xyz()
+
+        # and now to xyy (which is different somehow?)
+        self.xyy = self._xyz_to_xyy()
+        
+
+    def _linear_to_RGB(self):
+        """Convert linear rgb to RGB."""
+        # now we have to compand(?) it
+        RGB = numpy.empty(self.linear_rgb.shape)
+        mask = self.linear_rgb <= 0.0031308
+        RGB[mask] = 12.92 * self.linear_rgb[mask]
+        RGB[~mask] = 1.055 * self.linear_rgb[~mask]**(1/2.4) - 0.055
+
+        # and clip the values
+        RGB[RGB >= 1] = 1
+        RGB[RGB <= 0] = 0
+
+        return numpy.round_(RGB * 255)
+
+    def _RGB_to_linear(self):
+        """Convert RGB to linear rgb."""
+        rgb = numpy.empty(self.rgb.shape)
+        # make sure to normalise it first
+        RGB = self.rgb / 255
+
+        # make the conversion
+        mask = RGB > 0.04045
+        rgb[mask] = ((RGB[mask] + 0.055) / 1.055)**(2.4)
+        rgb[~mask] = RGB[~mask] / 12.92
+
+        return rgb
+
+    def _rgb_to_xyz(self):
+        """Convert linear rgb to xyz."""
+        return numpy.linalg.inv(_xyz_srgb_matrix) @ self.linear_rgb
+
+    def _xyz_to_xyy(self):
+        """Convert xyz to xyy."""
+        # we need the sum of the values
+        norm = self.xyz.sum()
+
+        # check if we're dealing with black
+        if norm == 0:
+            return numpy.array([0, 0, self.xyz[1]])
+
+        # otherwise we need to make the following transformation
+        return numpy.array([self.xyz[0] / norm, self.xyz[1] / norm, self.xyz[1]])
+
+    def as_though(self, condition, anomalise=False, _hex=False):
+        """Return the colour as though the condition."""
+        # first check something
+        if condition == "normal":
+            if _hex:
+                return "#{:02x}{:02x}{:02x}".format(*self.rgb.astype(numpy.int32))
+            return self.rgb
+        elif condition == "achroma":  # this one is special as it's the lack of colour
+            # make the grey point
+            z = (numpy.array([0.212656, 0.715158, 0.072186]) * self.rgb).sum() * numpy.ones(self.rgb.size)
+            if anomalise:
+                v = 1.75
+                n = v + 1
+                z = (v * z + self.rgb) / n
+
+            z = numpy.round_(z).astype(numpy.int32)
+
+            if _hex:
+                return "#{:02x}{:02x}{:02x}".format(z[0], z[1], z[2])
+            else:
+                return z
+
+        # now for the others
+        # get the style of colour blindness
+        style = _blindness_type[condition]
+
+        # we can get the confusion line by finding the slope from our source colour in xyy and the known
+        # confusion points from the colour blindness type
+        confuse_slope = (self.xyy[1] - style["y"]) / (self.xyy[0] - style["x"])
+
+        # from this we extract the y-intercept
+        y_int = self.xyy[1] - self.xyy[0] * confuse_slope
+
+        # now we find the change in x and y resulting from the confusion line being different to the
+        # colour axis for the blindness type
+        dx = (style["yi"] - y_int) / (confuse_slope - style["m"])
+        dy = (confuse_slope * dx) + y_int  # the change in y should just be propto the change in x
+        dY = 0  # not sure what this is yet, I believe it's the reference white point?
+
+        # now we can find the simulated colours in XYZ (from the white point?)
+        z = self.xyy[2] * numpy.array([dx / dy, 1, (1 - (dx + dy)) / dy])
+
+        # we then calculate the distance this colour is from the neutral grey in XYZ
+        dX = 0.312713 * self.xyy[2] / 0.329016 - z[0]
+        dZ = 0.358271 * self.xyy[2] / 0.329016 - z[2]
+
+        # convert the distance into linear rgb, and also convert the simulated colour
+        distance = _xyz_srgb_matrix @ numpy.array([dX, dY, dZ]).T
+        new_rgb = _xyz_srgb_matrix @ z.T
+
+        # the next thing to do is to figure out how to make the adjustment to our new_rgb
+        # first find the ratio of the rgb to the distance
+        ratio = ((new_rgb >= 0) - new_rgb) / distance  # not quite sure what this is...
+        ratio[(ratio < 0) | (ratio > 1)] = 0  # remove blown out values
+
+        # the adjustment factor we use is just the largest of these factors
+        adjustment = ratio.max()
+
+        # apply the shift
+        new_rgb += adjustment * distance
+
+        # and apply the companding
+        new_rgb[new_rgb < 0] = 0  # remove the blown out values
+        new_rgb[new_rgb > 1] = 1
+        new_rgb = new_rgb**(1/2.2)  # this is the gamma correction
+        new_rgb = (255 * new_rgb)
+
+        # and finally apply any anomalise correction to it
+        if anomalise:
+            v = 1.75
+            n = v + 1
+            new_rgb = (v * new_rgb + self.rgb) / n
+
+        new_rgb = numpy.round_(new_rgb).astype(numpy.int32)
+
+        if _hex:
+            return "#{:02x}{:02x}{:02x}".format(*new_rgb)
+        else:
+            return new_rgb
+
 
 class Colour():
     """The colour object."""
@@ -186,6 +342,7 @@ class Colour():
 
         # perform the in-situ conversion between the colours
         self.xyz = self._to_xyz()
+        self.linear_rgb = self._to_linear_rgb()
         self.rgb = self._to_rgb()
         self.lab = numpy.array(value)  # using the list in case it's a tuple
 
@@ -204,6 +361,11 @@ class Colour():
             return t**3
         else:
             return 3 * delta**2 * (t - 4 / 29)
+
+    def _to_linear_rgb(self):
+        """Return the linear rgb representation."""
+        rgb = self.xyz_matrix @ self.xyz
+        return rgb
 
     def _to_rgb(self):
         """Return the RGB representation of the colour."""
@@ -255,19 +417,19 @@ class ColourScheme():
     def set_chroma_limit(self, a, b):
         """Set the limits on the chroma scale."""
         assert(a <= b)
-        
+
         self.chroma_limit = [a, b]
-    
+
     def set_hue_limit(self, a, b):
         """Set the limits on the hue scale."""
         assert(a <= b)
-        
+
         self.hue_limit = [a * numpy.pi / 180, b * numpy.pi / 180]
-    
+
     def set_light_limit(self, a, b):
         """Set the limits on the light scale."""
         assert(a <= b)
-        
+
         self.light_limit = [a, b]
 
     def _hcl_lab_limits(self):
@@ -282,7 +444,7 @@ class ColourScheme():
 
         # the max is just the max in the values
         a_max_scale = max(numpy.cos(self.hue_limit[0]),
-                          numpy.cos(self.hue_limit[1])) 
+                          numpy.cos(self.hue_limit[1]))
 
         # now check the sign of the interval
         if numpy.cos(self.hue_limit[0]) * numpy.cos(self.hue_limit[1]) > 0:
@@ -312,7 +474,7 @@ class ColourScheme():
             b_max_scale = 1
         else:
             b_max_scale = max(numpy.sin(self.hue_limit[0]),
-                              numpy.sin(self.hue_limit[1])) 
+                              numpy.sin(self.hue_limit[1]))
 
         # now check the sign of the interval
         if numpy.sin(self.hue_limit[0]) * numpy.sin(self.hue_limit[1]) > 0:
@@ -355,7 +517,7 @@ class ColourScheme():
         # now convert those points into Colour objects
         colours = [Colour(i) for i in lab_values]
 
-        return colours 
+        return colours
 
     def get_rgb(self):
         """Print the RGB values of the colours in the scheme."""
@@ -405,7 +567,7 @@ def _interval_testing():
 
 
 if __name__ == "__main__":
-    
+
     # _visualise_movement()
     points = Points(20, periodic=True)
     for i in range(200):
